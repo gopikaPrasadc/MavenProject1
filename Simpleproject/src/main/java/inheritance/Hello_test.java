ed351520140a65cbb28a12d768dc85461c7777df
gopikaPrasadc <gopikacek14@gmail.com>
2023-06-15 13:08:44 +0530

    3rd commit

--- Simpleproject/src/main/java/inheritance/Hello_test.java
+++ Simpleproject/src/main/java/inheritance/Hello_test.java
@@ -2,6 +2,6 @@ package inheritance;
 
 public class Hello_test {
 public static void main(String[] args) {
-	System.out.println("This is my second commit");
+	System.out.println("This is my third commit");
 }
 }
